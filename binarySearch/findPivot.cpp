af2095d8f2203240b1b7955240852082ade0b69c
Archit Singh <architsingh456@gmail.com>
2021-02-05 19:33:06 +0530

    UPD : added solutions to problem on binary search

---
+++ binarySearch/findPivot.cpp
@@ -0,0 +1,50 @@
+/* What is a pivot in a rotated and sorted array ? */
+
+/* -- if(arr[i]>arr[i+1]) in a rotated and sorted array, then arr[i] is the pivot element */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//this function will return the index of the pivot element
+//if the array is totally sorted and there is no pivot element, then this will return -1
+int findPivot(vector<int> v,int low,int high){
+    if(low>high)
+        return -1;
+    
+    if(low==high)
+        return low;
+
+    int mid=low+(high-low)/2;
+
+    //checking if the current index is the pivot element
+    if(mid<high && v[mid]>v[mid+1])
+        return mid;
+    
+    //checking if the previous index is the pivot element
+    if(mid>low && v[mid]<v[mid-1])
+        return mid-1;
+
+    //if we have not found the pivot element yet, then recursively call for the two subarrays accordingly
+    if(v[low]<=v[mid])
+        return findPivot(v,mid+1,high);
+
+    //else we will find out in the left part
+    return findPivot(v,low,mid-1);
+}
+
+int main(){
+    
+    int n;cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i];
+
+    int pos=findPivot(v,0,n-1);
+
+    if(pos==-1)
+        cout<<"The array is already sorted, no pivot element found";
+
+    cout<<v[pos];
+
+    return 0;
+}
---
+++ binarySearch/searchInBitonicArray.cpp
@@ -0,0 +1,53 @@
+int asc(vector<int> A, int low, int high, int B) 
+{ 
+   while (low <= high) 
+   { 
+       int mid = low + (high - low) / 2; 
+       if (A[mid] == B) 
+           return mid; 
+       else if (A[mid] > B) 
+           high = mid - 1; 
+       else
+           low = mid + 1; 
+   } 
+    return -1; 
+} 
+  
+// Function for binary search in descending part of array 
+int dsc(vector<int>A, int low, int high, int B) 
+{ 
+   while (low <= high) 
+   { 
+       int mid = low + (high - low) / 2; 
+       if (A[mid] == B) 
+           return mid; 
+       else if (A[mid] < B) 
+           high = mid - 1; 
+       else
+           low = mid + 1; 
+   } 
+    return -1; 
+} 
+
+int Solution::solve(vector<int> &A, int B) {
+    int ans = -1;
+    int start = 0, end = A.size() - 1;
+    int mid = 0;
+    while(start<=end)
+    {
+        mid = start + (end-start)/2;
+        if(A[mid]>A[mid+1] && A[mid]>A[mid-1])
+            break;
+        else if(A[mid]<A[mid+1] && A[mid-1]<A[mid])
+            start = mid+1;
+        else
+            end = mid-1;
+    }
+    if(A[mid] == B)
+        return mid;
+    int l = asc(A,0,mid-1,B);
+    int r = dsc(A,mid+1,A.size()-1,B);
+    if(l!=-1 || r!=-1)
+        return l!=-1?l:r;
+    return -1;
+}
