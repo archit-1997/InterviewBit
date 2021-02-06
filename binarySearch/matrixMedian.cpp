89a872b482c0cc243d5f211f72682acbbe4a51bb
Archit Singh <architsingh456@gmail.com>
2021-02-06 23:52:29 +0530

    UPD : added solutions to problems on binary searh, trees ans stack

---
+++ binarySearch/matrixMedian.cpp
@@ -0,0 +1,31 @@
+
+int Solution::findMedian(vector<vector<int> > &A) {
+    int r=A.size(),c=A[0].size();
+    
+    //find the min and max element of the matrix
+    int big=INT_MIN,small=INT_MAX;
+    
+    for(int i=0;i<r;i++){
+        small=min(small,A[i][0]);
+        big=max(big,A[i][c-1]);
+    }
+    
+    int low=small,high=big;
+    int req=(r*c+1)/2;
+    while(low<high){
+        int sum=0;
+        int mid=low+(high-low)/2;
+        for(int i=0;i<r;i++){
+            vector<int> v=A[i];
+            sum+=(upper_bound(v.begin(),v.end(),mid)-v.begin());
+        }
+        
+        if(sum<req)
+            low=mid+1;
+        else
+            high=mid;
+    }
+    
+    return low;
+    
+}
