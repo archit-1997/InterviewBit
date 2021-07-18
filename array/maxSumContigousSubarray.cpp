742298e4c2e4d0c79c7d59609338746ca1c4ab72
archit-1997 <architsingh456@gmail.com>
2021-07-18 22:32:29 +0530

    UPD : added solution for maxSumContigousSubarray.cpp

--- array/maxSumContigousSubarray.cpp
+++ array/maxSumContigousSubarray.cpp
@@ -1,13 +1,10 @@
 int Solution::maxSubArray(const vector<int> &A) {
-
-  int meh = 0, msf = INT_MIN, n = A.size();
-
-  for (int i = 0; i < n; i++) {
-    meh += A[i];
-    msf = max(msf, meh);
-    if (meh < 0)
-      meh = 0;
-  }
-
-  return msf;
+    int msf=INT_MIN,meh=0;
+    for(int i=0;i<A.size();i++){
+        meh+=A[i];
+        msf=max(msf,meh);
+        if(meh<0)
+            meh=0;
+    }
+    return msf;
 }
