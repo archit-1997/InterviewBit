6f5655ce0c9c8dd1b2433588f47da5aa7f5860d7
archit-1997 <architsingh456@gmail.com>
2021-07-26 07:56:47 +0530

    UPD : updated solutions for Merge Intervals and Merge Overlapping Intervals

--- array/mergeOverlappingIntervals.cpp
+++ array/mergeOverlappingIntervals.cpp
@@ -7,29 +7,35 @@
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
-bool compare(Interval &a, Interval &b) { return a.start < b.start; }
 
-vector<Interval> Solution::merge(vector<Interval> &v) {
-  // Do not write main() function.
-  // Do not read input, instead use the arguments to the function.
-  // Do not print the output, instead return values as specified
-  // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for
-  // more details
+bool compare(Interval &a, Interval &b){
+    if(a.start==b.start)
+        return a.end<b.end;
+    return a.start<b.start;
+}
 
-  sort(v.begin(), v.end(), compare);
-  int n = v.size();
-  vector<Interval> ans;
-  for (int i = 0; i < n - 1; i++) {
-    if (v[i].end >= v[i + 1].start) {
-      // we keep on going forward with the proper values
-      v[i + 1].start = min(v[i].start, v[i + 1].start);
-      v[i + 1].end = max(v[i].end, v[i + 1].end);
-    } else
-      ans.push_back(v[i]);
-  }
-
-  // push the last pair
-  ans.push_back({v[n - 1].start, v[n - 1].end});
-
-  return ans;
+vector<Interval> Solution::merge(vector<Interval> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    vector<Interval> intervals=A;
+    sort(intervals.begin(),intervals.end(),compare);
+    int n=intervals.size();
+    int start=intervals[0].start,end=intervals[0].end;
+    vector<Interval> ans;
+    Interval newInterval;
+    for(int i=1;i<n;i++){
+        if(intervals[i].start<=end){
+                end=max(end,intervals[i].end);
+            }
+            else{
+                newInterval.start=start,newInterval.end=end;
+                ans.push_back(newInterval);
+                start=intervals[i].start,end=intervals[i].end;
+            }
+    }
+    newInterval.start=start,newInterval.end=end;
+    ans.push_back(newInterval);
+    return ans;
 }
